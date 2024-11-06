0279ca93aac2dc529803151c081b1d1267c54556
Gael <gael.lopez22@uabc.edu.mx>
2024-11-06 14:11:32 -0700

    Act ficha Tridomino

--- Jugador.java
+++ Jugador.java
@@ -1,2 +1,25 @@
+import java.util.ArrayList;
+
 public class Jugador {
+
+    private String nombre;
+    private int puntuacion;
+    private ArrayList<Ficha> mano;
+
+    public Jugador(String nombre)
+    {
+        this.nombre = nombre;
+        this.puntuacion = 0;
+        mano = new ArrayList<>();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
 }
