2f44d1136ca8774f1724096a2f127c332f03d956
Gael <gael.lopez22@uabc.edu.mx>
2024-11-06 09:49:04 -0700

    Act ficha Tridomino

--- FichaTridomino.java
+++ FichaTridomino.java
@@ -14,18 +14,27 @@ public class FichaTridomino extends Ficha implements Movible {
         this.lado3 = lado3;
     }
 
+    public boolean esMula()
+    {
+        if (super.getLado1() == super.getLado2() && lado3 == super.getLado1()) {
+            return true;
+        }
+        return false;
+    }
 
+    public int getLado3() {
+        return lado3;
+    }
 
+    public void setLado3(int lado3) {
+        this.lado3 = lado3;
+    }
 
+    public int getLado2() {
+        return super.getLado2();
+    }
 
-
-
-
-
-
-
-
-
-
-
+    public void setLado2(int lado2) {
+        super.setLado2(lado2);
+    }
 }
